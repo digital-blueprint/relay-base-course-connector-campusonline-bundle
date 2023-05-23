89714c2cc3cf52fd117b55ec988f31f42842f770
Christoph Reiter <reiter.christoph@gmail.com>
2023-05-23 13:18:26 +0200

    Use cache.app for the bundle cache

--- src/DependencyInjection/DbpRelayBaseCourseConnectorCampusonlineExtension.php
+++ src/DependencyInjection/DbpRelayBaseCourseConnectorCampusonlineExtension.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Dbp\Relay\BaseCourseConnectorCampusonlineBundle\DependencyInjection;
 
 use Dbp\Relay\BaseCourseConnectorCampusonlineBundle\EventSubscriber\CourseEventSubscriber;
-use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
@@ -21,13 +20,7 @@ class DbpRelayBaseCourseConnectorCampusonlineExtension extends ConfigurableExten
         );
         $loader->load('services.yaml');
 
-        $courseCache = $container->register('dbp_api.cache.course.campus_online', FilesystemAdapter::class);
-        $courseCache->setArguments(['relay-base-course-connector-campusonline', 60, '%kernel.cache_dir%/dbp/relay-base-course-connector-campusonline']);
-        $courseCache->setPublic(true);
-        $courseCache->addTag('cache.pool');
-
         $courseApi = $container->getDefinition('Dbp\Relay\BaseCourseConnectorCampusonlineBundle\Service\CourseApi');
-        $courseApi->addMethodCall('setCache', [$courseCache, 3600]);
         $courseApi->addMethodCall('setConfig', [$mergedConfig['campus_online'] ?? []]);
 
         $courseApi = $container->getDefinition(CourseEventSubscriber::class);
